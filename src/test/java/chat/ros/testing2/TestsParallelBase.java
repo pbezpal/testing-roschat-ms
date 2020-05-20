9b1523671548b96c7ec7859ca8b092658caa152b
pbezpal <bezpalko@infotek.ru>
2020-05-20 19:38:05 +0300

    Переделал проверку каналов после всех тестов

--- src/test/java/chat/ros/testing2/TestsParallelBase.java
+++ src/test/java/chat/ros/testing2/TestsParallelBase.java
@@ -2,16 +2,13 @@ package chat.ros.testing2;
 
 import chat.ros.testing2.server.administration.ChannelsPage;
 import com.codeborne.selenide.WebDriverRunner;
-import io.qameta.allure.Step;
 import org.testng.ITestContext;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
-import org.testng.asserts.SoftAssert;
 
 import java.lang.reflect.Method;
 
 import static chat.ros.testing2.data.ContactsData.*;
-import static com.codeborne.selenide.Selenide.sleep;
 import static org.testng.Assert.assertTrue;
 
 public interface TestsParallelBase {
@@ -20,11 +17,6 @@ public interface TestsParallelBase {
     String commandDBCheckChannel = "sudo -u roschat psql -c \"select * from channels;\" | grep '%1$s'";
     String commandDBCheckTypeChannel = commandDBCheckChannel + "| awk -F\"|\" '{print $2}'";
     String commandDBCheckProvedChannel = commandDBCheckChannel + "| awk -F\"|\" '{print $4}'";
-
-    @BeforeSuite
-    default void setUp(){
-        sleep(5000);
-    }
 
     @BeforeClass
     default void beforeClass(){
@@ -61,8 +53,7 @@ public interface TestsParallelBase {
     @AfterSuite
     default void tearDown(ITestContext c){
         ITestContext context = c;
-        if(context.getCurrentXmlTest().getName().contains("Channels")){
-            sleep(10000);
+        if(context.getCurrentXmlTest().getName().equals("Channels-Public-Proven")){
             testBase.init();
             testBase.openMS("/admin/channels");
             ChannelsPage channelsPage = new ChannelsPage();
