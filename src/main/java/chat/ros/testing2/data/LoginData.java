0077d039b0de0f9b7afac23bbc49c107c6b49de6
pbezpal <bezpalko@infotek.ru>
2020-06-30 20:27:44 +0300

    return host

--- src/main/java/chat/ros/testing2/data/LoginData.java
+++ src/main/java/chat/ros/testing2/data/LoginData.java
@@ -4,8 +4,7 @@ public interface LoginData {
 
     /****** Параметры сервера ******/
     String HOST_HUB = "10.10.199.45";
-    //String HOST_SERVER = "testing2.ros.chat";
-    String HOST_SERVER = "10.10.38.173";
+    String HOST_SERVER = "testing2.ros.chat";
     String PORT_SERVER = "1110";
 
     /****** Параметры для авторизации в СУ ******/
