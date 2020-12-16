b84134d286f76f82d659c154d522c3058813380a
pbezpal <bezpalko@infotek.ru>
2020-12-16 12:51:52 +0300

    fix: attach screenshot to report

--- src/main/java/chat/ros/testing2/helpers/AttachToReport.java
+++ src/main/java/chat/ros/testing2/helpers/AttachToReport.java
@@ -1,81 +1,27 @@
 package chat.ros.testing2.helpers;
 
 import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.SelenideElement;
 import com.codeborne.selenide.WebDriverRunner;
-import com.google.common.io.Files;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.logging.LogEntries;
 import org.openqa.selenium.logging.LogEntry;
 import org.openqa.selenium.logging.LogType;
-import ru.yandex.qatools.ashot.AShot;
-import ru.yandex.qatools.ashot.Screenshot;
 
-import javax.imageio.ImageIO;
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.List;
 
 public class AttachToReport {
 
+    public AttachToReport() {}
+
     @Attachment(type = "image/png")
-    public static byte[] AttachScreen(File screenshot) {
-        try {
-            return screenshot == null ? null : Files.toByteArray(screenshot);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    private static AShot aShot = new AShot();
-    private static String pathScreenshots = "./build/classes/screenshots/";
-
-    public static void AScreenshot(String filename){
-        Screenshot screenshot = aShot.takeScreenshot(WebDriverRunner.getWebDriver());
-
-        if( ! java.nio.file.Files.exists(Paths.get(pathScreenshots))) {
-            try {
-                java.nio.file.Files.createDirectories(Paths.get(pathScreenshots));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        File currentScreenshot = new File(pathScreenshots + filename + ".png");
-
-        try {
-            ImageIO.write(screenshot.getImage(), "png", currentScreenshot);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        AttachScreen(currentScreenshot);
-    }
-
-    public static void AScreenshot(String filename, SelenideElement element){
-        Screenshot screenshot = aShot.takeScreenshot(WebDriverRunner.getWebDriver(), element);
-
-        if( ! java.nio.file.Files.exists(Paths.get(pathScreenshots))) {
-            try {
-                java.nio.file.Files.createDirectories(Paths.get(pathScreenshots));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        File currentScreenshot = new File(pathScreenshots + filename + ".png");
-
-        try {
-            ImageIO.write(screenshot.getImage(), "png", currentScreenshot);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        AttachScreen(currentScreenshot);
+    public byte[] AttachScreen() {
+        return ((TakesScreenshot) WebDriverRunner.getWebDriver()).getScreenshotAs(OutputType.BYTES);
     }
 
     @Attachment(value = "Browser network log", type = "text/plain")
--- src/test/java/chat/ros/testing2/WatcherTests.java
+++ src/test/java/chat/ros/testing2/WatcherTests.java
@@ -1,19 +1,15 @@
 package chat.ros.testing2;
 
+import chat.ros.testing2.helpers.AttachToReport;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.TestWatcher;
 
-import static chat.ros.testing2.helpers.AttachToReport.*;
-
-public class WatcherTests implements TestWatcher {
+public class WatcherTests extends AttachToReport implements TestWatcher {
 
     @Override
     public void testFailed(ExtensionContext context, Throwable throwable) {
-        String filename = String.valueOf(context.getTestMethod());
-        if( ! context.getTestMethod().toString().contains("BD")) {
-            AScreenshot(filename);
-            ABrowserLogNetwork();
-            ABrowserLogConsole();
-        }
+        AttachScreen();
+        ABrowserLogNetwork();
+        ABrowserLogConsole();
     }
 }
